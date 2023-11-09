7f0efad498f95b7510831a4d388a5fc1ab859676
Nathan Lemoine <nl024482@chbah.org>
2023-11-09 14:17:02 +0100

    fix: Update database configuration for production environment

--- config/database.js
+++ config/database.js
@@ -1,17 +1,17 @@
 const path = require("path");
 
 module.exports = ({ env }) => {
-  const client = env("DATABASE_CLIENT");
+  const client = env("DATABASE_CLIENT", "mysql");
 
   const connections = {
     mysql: {
       connection: {
-        connectionString: env("DATABASE_URL"),
-        host: env("DATABASE_HOST"),
-        port: env.int("DATABASE_PORT"),
-        database: env("DATABASE_NAME"),
-        user: env("DATABASE_USERNAME"),
-        password: env("DATABASE_PASSWORD"),
+        connectionString: env("MYSQL_ADDON_URI", ""),
+        host: env("MYSQL_ADDON_HOST"),
+        port: env.int("MYSQL_ADDON_PORT"),
+        database: env("MYSQL_ADDON_DB"),
+        user: env("MYSQL_ADDON_USER"),
+        password: env("MYSQL_ADDON_PASSWORD"),
         ssl: env.bool("DATABASE_SSL", false) && {
           key: env("DATABASE_SSL_KEY", undefined),
           cert: env("DATABASE_SSL_CERT", undefined),
